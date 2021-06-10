1644024301bcc2a3636482c19887d603e99b4596
Bogdan T. Berghie <bogdan337@gmail.com>
2021-06-10 15:04:37 +0200

    prueba

--- API.php
+++ API.php
@@ -1,71 +1,73 @@
-<?php
+<?php 
 
-class JugaToysAPI
-{
-    private $options;
+class JugaToysAPI{
 
-    private $ch;
+  private $options;
 
-    /**
-     * Start up
-    */
-    public function __construct()
-    {
-        $this->options = get_option('jugatoys_settings');
-        if (!$this->options) {
-            throw new Exception(__("Es necesario introducir los ajustes del TPV", "jugatoys"), 1);
-        }
+  private $ch;
 
-        $this->ch = curl_init();
-        curl_setopt($this->ch, CURLOPT_HEADER, true);
-        curl_setopt($this->ch, CURLOPT_PORT, $this->options['puerto']);
-        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->options['timeout']);
-        // curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, 0);
+  /**
+   * Start up
+  */
+  public function __construct()
+  {
+    $this->options = get_option( 'jugatoys_settings' );
+    if (!$this->options) {
+      throw new Exception(__("Es necesario introducir los ajustes del TPV","jugatoys"), 1);
+    }
+
+    $this->ch = curl_init();
+    curl_setopt($this->ch, CURLOPT_HEADER, TRUE);
+    curl_setopt($this->ch, CURLOPT_PORT , $this->options['puerto']);
+    curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->options['timeout']);
+    // curl_setopt($tuCurl, CURLOPT_SSL_VERIFYPEER, 0);
 
     // curl_setopt($this->ch, CURLOPT_NOBODY, TRUE); // remove body
+  }
+
+  public function productInfo($arrayIds = array(), $updated_from = false){
+
+    $url = $this->options['url'] . '/productinfo';
+    $data = array();
+
+    if (!empty($arrayIds)) {
+      if (is_array($arrayIds)) {
+        $data['products-id'] = $arrayIds;
+      }else{
+        $data['products-id'] = array($arrayIds);
+      }
     }
 
-    public function productInfo($arrayIds = array(), $updated_from = false)
-    {
-        $url = $this->options['url'] . '/productinfo';
-        $data = array();
-
-        if (!empty($arrayIds)) {
-            if (is_array($arrayIds)) {
-                $data['products-id'] = $arrayIds;
-            } else {
-                $data['products-id'] = array($arrayIds);
-            }
-        }
-
-        if (!empty($updated_from)) {
-            $data['updated_from'] = $updated_from;
-        }
-
-        return $this->post($url, $data);
+    if (!empty($updated_from)) {
+      $data['updated_from'] = $updated_from;
     }
 
-    public function stockPrice($arrayIds = array(), $updated_from = false)
-    {
-        $url = $this->options['url'] . '/stockprice';
-        $data = array();
+    return $this->post($url, $data);
+    
+  }
 
-        if (is_array($arrayIds) && !empty($arrayIds)) {
-            $data['producto_id'] = $arrayIds;
-        }
+  public function stockPrice($arrayIds = array(), $updated_from = false){
 
-        if (!empty($updated_from)) {
-            $data['updated_from'] = $updated_from;
-        }
+    $url = $this->options['url'] . '/stockprice';
+    $data = array();
 
-        return $this->post($url, $data);
+    if (is_array($arrayIds) && !empty($arrayIds)) {
+      $data['producto_id'] = $arrayIds;
     }
 
-    public function ticketInsert($orderId, $typeCharge, $lines = array())
-    {
-        $url = $this->options['url'] . '/ticketinsert';
-        $data = array(
+    if (!empty($updated_from)) {
+      $data['updated_from'] = $updated_from;
+    }
+
+    return $this->post($url, $data);
+    
+  }
+
+  public function ticketInsert($orderId, $typeCharge, $lines = array()){
+
+    $url = $this->options['url'] . '/ticketinsert';
+    $data = array(
       'Ticket' => array(
         'OrderID' => absint($orderId),
         'Type_Charge' => $typeCharge,
@@ -73,36 +75,44 @@ class JugaToysAPI
       )
     );
 
-        if (is_array($lines) && !empty($lines)) {
-            $data['Ticket']['Ticket_Lines'] = $lines;
-        }
-
-        return $this->post($url, $data);
+    if (is_array($lines) && !empty($lines)) {
+      $data['Ticket']['Ticket_Lines'] = $lines;
     }
 
-    private function post($url, $params)
-    {
-        curl_setopt($this->ch, CURLOPT_URL, $url);
+    return $this->post($url, $data);
+    
+  }
 
-        $credenciales = array(
+  private function post($url, $params){
+    curl_setopt($this->ch, CURLOPT_URL, $url);
+
+    $credenciales = array(
       'user' => $this->options['usuario'],
       'password' => $this->options['pw']
     );
 
-        $data = array_merge($credenciales, $params);
-        $jsonData = json_encode($data);
+    $data = array_merge($credenciales, $params);
+    $jsonData = json_encode($data);
 
-        curl_setopt($this->ch, CURLOPT_POST, true);
-        curl_setopt($this->ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($this->ch, CURLOPT_POST, true);
+    curl_setopt($this->ch, CURLOPT_POSTFIELDS, $jsonData);
 
-        $respuesta = curl_exec($this->ch);
-        $httpCode = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
-        curl_close($this->ch);
+    $respuesta = curl_exec($this->ch);
+    $httpCode = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+    curl_close($this->ch);
 
-        if ($httpCode < 400) {
-            return json_decode($respuesta);
-        } else {
-            return false;
-        }
+    if($httpCode < 400)
+    {
+        return json_decode($respuesta);
     }
+    else
+    {
+        return FALSE;
+    }
+
+  }
+
 }
+
+
+?>
--- utilidades.php
+++ utilidades.php
@@ -1,114 +1,115 @@
-<?php
+<?php 
 
-function actualizarStockProductoActual()
-{
-    //Obtenemos objeto de producto actual
-    global $product;
+function actualizarStockProductoActual(){
+  //Obtenemos objeto de producto actual
+  global $product;
   
-    actualizarStockIdProducto($product->id);
-}
-function actualizarStockCron()
-{
-    
+  actualizarStockIdProducto($product->id);
 }
 
-function actualizarStockCarrito()
-{
-    //Obtenemos objeto woocommerce
-    global $woocommerce;
+function actualizarStockCarrito(){
+  //Obtenemos objeto woocommerce
+  global $woocommerce;
 
-    //Obtenemos productos en carrito
-    $products = $woocommerce->cart->get_cart();
+  //Obtenemos productos en carrito
+  $products = $woocommerce->cart->get_cart();
 
-    //Creamos array vacío para ids de productos
-    $aIdProducto = array();
-    foreach ($products as $item => $product) {
-        $aIdProducto[] = $product['product_id'];
-    }
+  //Creamos array vacío para ids de productos
+  $aIdProducto = array();
+  foreach($products as $item => $product) { 
+    $aIdProducto[] = $product['product_id'];
+  }
   
-    if (empty($aIdProducto)) {
-        return false;
-    }
+  if (empty($aIdProducto)) {
+    return false;
+  }
 
-    actualizarStockIdProducto($aIdProducto);
+  actualizarStockIdProducto($aIdProducto);
 }
 
 
-function actualizarStockIdProducto($aIdProducto = array())
-{
-    if (empty($aIdProducto)) {
-        return false;
+function actualizarStockIdProducto($aIdProducto = array()){
+
+  if (empty($aIdProducto)) {
+    return false;
+  }
+
+  if (!is_array($aIdProducto)) {
+    $aIdProducto = array($aIdProducto);
+  }
+
+  //Preparamos array 
+  $aProductId_Sku = array();
+
+  foreach ($aIdProducto as $key => $idProducto) {
+    //Obtenemos SKU del producto, para consultar contra la API
+    $sku = get_post_meta($idProducto, '_sku', true);
+    if (!empty($sku)) {
+      $aProductId_Sku[$idProducto] = $sku;
     }
+  }
 
-    if (!is_array($aIdProducto)) {
-        $aIdProducto = array($aIdProducto);
-    }
+  //Si no se han obtenido SKUs, devolvemos
+  if (empty($aProductId_Sku)) {
+    return false;
+  }
 
-    //Preparamos array
-    $aProductId_Sku = array();
-
-    foreach ($aIdProducto as $key => $idProducto) {
-        //Obtenemos SKU del producto, para consultar contra la API
-        $sku = get_post_meta($idProducto, '_sku', true);
-        if (!empty($sku)) {
-            $aProductId_Sku[$idProducto] = $sku;
-        }
-    }
-
-    //Si no se han obtenido SKUs, devolvemos
-    if (empty($aProductId_Sku)) {
-        return false;
-    }
-
-    //Enviamos array de sku y product id
-    actualizarStockSku($aProductId_Sku);
+  //Enviamos array de sku y product id
+  actualizarStockSku($aProductId_Sku);
+  
 }
 
 // $aProductId_Sku: array(productId => sku)
-function actualizarStockSku($aProductId_Sku = array())
-{
+function actualizarStockSku($aProductId_Sku = array()){
 
   //Si no llega aProductId_Sku, devolvemos
-    if (empty($aProductId_Sku)) {
-        return false;
-    }
+  if (empty($aProductId_Sku)) {
+    return false;
+  }
 
-    //Todo: VERIFICAR QUE FUNCIONA
+  //Todo: VERIFICAR QUE FUNCIONA  
 
-    //Iniciamos API
-    $api = new JugaToysAPI();
+  //Iniciamos API
+  $api = new JugaToysAPI();
 
-    //Consultamos contra la API
-    $skus = array_values($aProductId_Sku);
-    $productInfo = $api->productInfo($skus);
+  //Consultamos contra la API
+  $skus = array_values($aProductId_Sku);
+  $productInfo = $api->productInfo($skus);
 
-    if (empty($productInfo)) {
-        return false;
-    }
+  if (empty($productInfo)) {
+    return false;
+  }
 
-    //Verificamos si la respuesta es correcta
-    if ($productInfo->Result == "OK") {
+  //Verificamos si la respuesta es correcta
+  if ($productInfo->Result == "OK") {
 
     //Verificamos que haya datos
-        if (!empty($productInfo->Data)) {
+    if (!empty($productInfo->Data)) {
 
       //Si solo devuelve un dato, quizá devuelva el objeto directamente en vez de array. Confirmamos, y convertimos en array si es preciso para que encaje en lógica
-            if (!is_array($productInfo->Data)) {
-                $productInfo->Data = array($productInfo->Data);
-            }
+      if (!is_array($productInfo->Data)) {
+        $productInfo->Data = array($productInfo->Data);
+      }
 
-            //Recorremos todos los productos devueltos
-            foreach ($productInfo->Data as $key => $pData) {
-                if (!empty($pData->Sku_Provider)) {
-                    //Confirmamos que hayamos solicitado el SKU
-                    $idProducto = array_search($pData->Sku_Provider, $aProductId_Sku);
-                    if ($idProducto !== false) {
-                        //Si coincide el SKU, actualizamos stock
-                        $stock = absint($pData->Stock);
-                        wc_update_product_stock($idProducto, $stock, 'set');
-                    }
-                }
-            }
+      //Recorremos todos los productos devueltos
+      foreach ($productInfo->Data as $key => $pData) {
+
+        if (!empty($pData->Sku_Provider) ) {
+          //Confirmamos que hayamos solicitado el SKU
+          $idProducto = array_search($pData->Sku_Provider, $aProductId_Sku);
+          if ($idProducto !== false) {
+            //Si coincide el SKU, actualizamos stock
+            $stock = absint($pData->Stock);
+            wc_update_product_stock( $idProducto,  $stock , 'set' );
+          }
+
         }
+
+      }
+
     }
+  }
 }
+
+
+ ?>
