30207edaabfa3c6de96d628ca2d72686200d9ff2
Bogdan T. Berghie <bogdan337@gmail.com>
2021-10-07 19:20:30 +0200

    cambio de version 1.3.7 a 1.3.7.1 para que me deje subir el pluguin el wordpress

--- jugatoys.php
+++ jugatoys.php
@@ -3,7 +3,7 @@
 Plugin Name: JugaToys
 Plugin URI: https://serinfor.net
 Description: Plugin para guardar datos de presupuestos automáticos. 
-Version: 1.3.7
+Version: 1.3.7.1
 Author: Jon Alain Hinojosa & Bogdan Berghie
 Author URI: https://serinfor.net
 License: GPL2
